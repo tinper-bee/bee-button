375202afa5d9cfaed2181bc5482534fb39ff53cc
Boyuzhou <386607913@qq.com>
2016-10-18 10:42:46 +0800

    ver. 0.0.6

--- build/index.js
+++ build/index.js
@@ -3,12 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Button = undefined;
 
-var _Button2 = require('./Button');
+var _Button = require('./Button');
 
-var _Button3 = _interopRequireDefault(_Button2);
+var _Button2 = _interopRequireDefault(_Button);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-exports.Button = _Button3["default"];
+exports["default"] = _Button2["default"];
+module.exports = exports['default'];

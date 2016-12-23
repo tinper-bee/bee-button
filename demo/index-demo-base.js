61743cd677dea1d4cf63c50163b8d6d84b45981b
Boyuzhou <386607913@qq.com>
2016-12-23 10:52:42 +0800

    change active style

--- demo/index-demo-base.js
+++ demo/index-demo-base.js
@@ -37,23 +37,12 @@ class Demo extends Component {
             </Button>
         );
 
-        const header = (
-            <Row>
-                <Col md={11}>
-                { example }
-                </Col>
-                <Col md={1}>
-                <Button shape="icon" onClick={ this.handleClick }>
-                    { caret }
-                </Button>
-                </Col>
-            </Row>
-        );
+
         return (
             <Col md={12} >
                 <h3>{ title }</h3>
                 <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ header } footer={footer} footerStyle = {{padding: 0}}>
+                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0}}>
                     <pre><code className="hljs javascript">{ code }</code></pre>
                 </Panel>
             </Col>

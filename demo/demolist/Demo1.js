a808ca191f07fd53c7f2d46d1feffcf2b3820e0d
liushzh <liushzh@yonyou.com>
2017-12-11 10:01:48 +0800

    feat(bee-button): 增加 isSubmit props
    增加 isSubmit props

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -12,7 +12,7 @@ export default class Demo1 extends Component {
     render () {
         return (
             <div className="demoPadding">
-                <Button>Default</Button>
+                <Button isSubmit={true}>Default</Button>
                 <Button disabled>disabled</Button>
                 <Button shape="border">border</Button>
                 <Button shape="round">round</Button>
